6ed365afee552e51d428b31f76830e1ad2538191
Crenchi <christopherlambrinos@gmail.com>
2022-12-07 01:16:02 +0100

    summary
    Revert back to basic website

---
+++ script.js
@@ -0,0 +1,16 @@
+let button = document.querySelector("button");
+    button.style.backgroundColor = 'white';
+
+window.onload = function (){
+    button.addEventListener("click", () => {
+        const backgroundColor = button.style.backgroundColor;
+
+        if (backgroundColor === 'salmon') {
+            button.style.backgroundColor = 'green';
+        } else if (backgroundColor === 'green') {
+            button.style.backgroundColor = 'purple';
+        } else {
+            button.style.backgroundColor = 'salmon';
+        }
+      });
+    }
