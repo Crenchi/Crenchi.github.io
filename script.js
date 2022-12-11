a1bb0ebff2fd77217bb96f17cd7f012df50f2442
Crenchi <christopherlambrinos@gmail.com>
2022-12-11 03:30:05 +0100

    fixed format, commented out vibration

--- script.js
+++ script.js
@@ -2,29 +2,29 @@ let audio = document.getElementById("audio");
 var vibrator = 0;
 var vibrating = Boolean(true);
 const alert_number = 10;
-const pattern = [100,300,100,100,100,500]
-anime = function(){
-  addclasses();
-  playPauseMusic();
-  vibration();
+const pattern = [100, 300, 100, 100, 100, 500]
+
+anime = function () {
+    addclasses();
+    playPauseMusic();
+    //vibration();
 }
 
-addclasses = function(){
-    navigator.vibrate([100,300,100,100,100,500]);
-  let heart = document.querySelector('.heart')
-  heart.classList.toggle('heartanime')
-  let back = document.querySelector('.back')
-  back.classList.toggle('backanime')
-  let in2 = document.querySelector('#in')
-  in2.classList.toggle('inanime')
-  let hold = document.querySelector('#hold')
-  hold.classList.toggle('holdanime')
-  let out = document.querySelector('#out')
-  out.classList.toggle('outanime')
+addclasses = function () {
+    let heart = document.querySelector('.heart')
+    heart.classList.toggle('heartanime')
+    let back = document.querySelector('.back')
+    back.classList.toggle('backanime')
+    let in2 = document.querySelector('#in')
+    in2.classList.toggle('inanime')
+    let hold = document.querySelector('#hold')
+    hold.classList.toggle('holdanime')
+    let out = document.querySelector('#out')
+    out.classList.toggle('outanime')
 }
 
-playPauseMusic = function(){
-    if(!audio.paused){
+playPauseMusic = function () {
+    if (!audio.paused) {
         audio.pause();
         audio.currentTime = 0;
     } else {
@@ -32,8 +32,8 @@ playPauseMusic = function(){
     }
 }
 
-vibration = function(){
-    if(vibrating){
+vibration = function () {
+    if (vibrating) {
         navigator.vibrate(0);
         vibrating = false;
     } else {
