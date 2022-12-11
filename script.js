dcdd0a58995ac3ec7185d8d87b9dbf8ac5c018c3
Crenchi <christopherlambrinos@gmail.com>
2022-12-11 02:24:15 +0100

    fixed animation on vscode

--- script.js
+++ script.js
@@ -5,12 +5,16 @@ anime = function(){
 }
 
 addclasses = function(){
-  $('.heart').toggleClass('heartanime')
-  $('.back').toggleClass('backanime')
-  $('#in').toggleClass('inanime')
-  $('#hold').toggleClass('holdanime')
-  $('#out').toggleClass('outanime')
-
+  let heart = document.querySelector('.heart')
+  heart.classList.toggle('heartanime')
+  let back = document.querySelector('.back')
+  back.classList.toggle('backanime')
+  let in2 = document.querySelector('#in')
+  in2.classList.toggle('inanime')
+  let hold = document.querySelector('#hold')
+  hold.classList.toggle('holdanime')
+  let out = document.querySelector('#out')
+  out.classList.toggle('outanime')
   //if(!audio.paused){
     //audio.pause();
     //audio.currentTime = 0;}
