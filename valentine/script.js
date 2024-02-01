73f82adc061c87b6dec16e038bdc40b90e6b9cda
Crenchi <christopherlambrinos@gmail.com>
2024-02-01 14:34:55 +0100

    fixed buttons and text

--- valentine/script.js
+++ valentine/script.js
@@ -11,8 +11,6 @@ const noClick = () => {
     const newLeft = Math.random() * (viewportWidth - button.offsetWidth);
     const newTop = Math.random() * (viewportHeight - button.offsetHeight);
 
-    console.log(newLeft);
-    console.log(newTop);
     button.style.left = `${newLeft}px`;
     button.style.top = `${newTop}px`;
   };
