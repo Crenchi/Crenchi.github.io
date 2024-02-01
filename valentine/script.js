1df2c7d0ea9938628d353bd642083fb3f351404a
Crenchi <christopherlambrinos@gmail.com>
2024-02-01 15:14:55 +0100

    added image

--- valentine/script.js
+++ valentine/script.js
@@ -18,6 +18,7 @@ const noClick = () => {
   const yesClick = () => {
     audio = document.getElementById("AboSo");
     playPauseMusic()
+    showImage()
   };
 
 
@@ -34,3 +35,29 @@ const noClick = () => {
     audio.currentTime = 0;
     audio.play();
   }
+
+  function changeText(buttonId, newText) {
+    var button = document.getElementById(buttonId);
+    button.innerHTML = newText;
+}
+const sad = ":("
+// Event listeners for the No button
+var noButton = document.getElementById('No');
+noButton.addEventListener('mouseover', function () {
+    changeText('No', sad);
+});
+noButton.addEventListener('mouseout', function () {
+    changeText('No', 'No');
+});
+
+
+function showImage() {
+  // Get the image element
+  var image = document.getElementById('selfie');
+
+  // Set the source of the image
+  image.src = '/Audio/us.jpeg';
+
+  // Display the image container
+  document.getElementById('imageContainer').style.display = 'block';
+}
