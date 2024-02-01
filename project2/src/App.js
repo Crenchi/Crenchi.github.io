0f911767827e059e41097c3bc117fed8a1cd04c5
Crenchi <christopherlambrinos@gmail.com>
2024-02-01 01:28:05 +0100

    minor bug in go back

--- project2/src/App.js
+++ project2/src/App.js
@@ -3,7 +3,7 @@ import TimersDashboard from './timer';
 function App() {
   return (
     <>
-    <button class="ui button absolute-top-left" onClick={window.history.back}>Go back</button>
+    <button class="ui button absolute-top-left" onClick={window.history.back()}>Go back</button>
     <TimersDashboard/>
     </>
   );
